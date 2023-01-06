3685e9d4ef22b1f9dd7d54856059127d7794f38e
shivam jain <113499149+shivamj2020@users.noreply.github.com>
2023-01-06 12:59:48 +0530

    Update Settings.js

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,16 +1,22 @@
-import React from "react"
-import { useContext } from "react"
-import { UserContext } from "../context/userContext"
+import React from "react";
+import { useContext, useState } from "react";
+import { UserContext } from "../context/userContext";
 
 export const Settings = () => {
+    const [greeting, setGreeting] = useState("");
 
     // to be implemented in context
-    const { changeGreeting } = useContext(UserContext)
+    const { changeGreeting } = useContext(UserContext);
+
+    const handleInput = () => {
+        // e.preventDefault();
+        changeGreeting(greeting);
+    };
 
     return (
-        <div style={{ border: '5px solid red', padding: '8px' }} id="settings">
+        <div style={{ border: "5px solid red", padding: "8px" }} id="settings">
             <h4>Settings</h4>
-            <input type={'text'} />
+            <input type={"text"} onChange={(e) => { setGreeting(e.target.value) }} onKeyUp={handleInput} />
         </div>
-    )
-}
+    );
+};
